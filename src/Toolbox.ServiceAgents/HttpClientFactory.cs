142cbef1d064d6922441d18deab9315dff47c3f2
WillemDN <Willem.de.nys@gmail.com>
2016-03-09 09:37:10 +0100

    onnodige usings verwijderd

--- src/Toolbox.ServiceAgents/HttpClientFactory.cs
+++ src/Toolbox.ServiceAgents/HttpClientFactory.cs
@@ -3,7 +3,6 @@ using Microsoft.Extensions.OptionsModel;
 using System;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Text;
 using Toolbox.ServiceAgents.Settings;
 
 namespace Toolbox.ServiceAgents
